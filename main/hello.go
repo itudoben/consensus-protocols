d19b6b94b9fc6eaad221de9e66f4396e51b07746
Johnny Hujol <johnny.hujol@siren.io>
2023-03-03 18:34:39 +0100

    refactor: added error handling and test
    Signed-off-by: Johnny Hujol <johnny.hujol@siren.io>

--- main/hello.go
+++ main/hello.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
+    "log"
 
+    // go mod edit -replace example.com/greetings=../greetings
+    // It allows to use the local code when developing and before publishing it.
 	"itudoben.io/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
-	message := greetings.Hello("Gladys")
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(log.Ldate | log.Ltime)
+
+    log.Print("testing the multiple return values function")
+	message, err := greetings.Hello("George")
+
+	if err != nil {
+	    log.Fatal(err)
+	}
+
 	fmt.Println(message)
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
